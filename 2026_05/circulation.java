4975276b8f1576b5d3e715df3e4b8815859a8329
kid <1774083118@qq.com>
2026-05-19 20:07:45 +0800

    java cir practice

---
+++ 2026_05/circulation.java
@@ -0,0 +1,23 @@
+public class circulation {
+    public static void main(String[] args){
+        int x=10;
+        while (x<20){
+            System.out.print("value of x"+x);
+            x++;
+            System.out.print("\n");
+            
+        }
+        do{
+            System.out.print("value of x"+x);
+            x++;
+            System.out.print("\n");
+        }while(x<20);
+
+        for(int x = 10; x < 20; x = x+1) {
+            System.out.print("value of x : " + x );
+            System.out.print("\n");
+        }
+    }
+
+    }
+}
