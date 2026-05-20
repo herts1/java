2aeb4aa1670aab6b2ef618f23d057da37e24ebac
kid <1774083118@qq.com>
2026-05-20 17:56:29 +0800

    String practice

---
+++ 2026_05/StringDemo.java
@@ -0,0 +1,23 @@
+public class StringDemo {
+    public static void main(String args[]){
+        char[] helloArray={'r','u','n','o','o','b'};
+        String helloString=new String(helloArray);
+        System.out.println(helloString+'\n');
+
+        String site = "kskbl.zdjd";
+        int len = site.length();
+        System.out.println("EDG.KK:"+len);
+        // 计算字符串长度
+
+        String string1 = "wqzkbl";
+        System.out.println("EDG.kk"+String1+"mpzswpwzsbll");
+        String String3 = String1.concat(String2);
+        System.out.println(String3 );
+        //连接字符串
+        
+
+
+
+
+    }
+}
--- 2026_05/circulation.java
+++ 2026_05/circulation.java
@@ -18,6 +18,7 @@ public class circulation {
             System.out.print("\n");
         }
     }
+    
 
     }
 }
